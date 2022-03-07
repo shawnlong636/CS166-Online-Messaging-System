3044f341e5e345a3e5d9d7985fc53884298eabe5
Shawn Long <slong024@ucr.edu>
2022-03-06 22:37:09 -0800

    Updated create_db script to actually create the databse

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "creating db named ... "$USER"_DB"
+createdb -h localhost -p $PGPORT $USER"_DB"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
+psql -h localhost -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
+psql -h localhost -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
+psql -h localhost -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
