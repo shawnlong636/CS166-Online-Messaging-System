bc05e828443709301ed87f20d46dbeba82b46604
S-Kurisu <62637568+S-Kurisu@users.noreply.github.com>
2022-03-07 17:46:00 -0800

    Moving files to main
    moving files

---
+++ java/src/ChangePassword.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ChangePassword extends SubScreen
+{
+	/**
+	 * Default Constructor for CSDepartment subclass
+	 */
+	ChangePassword()
+	{
+		
+	}
+	/**
+	 * A method that reads in the info from the CS text file
+	 * to display in the main text area as information for the 
+	 * CS department
+	 */
+	public void display()
+	{
+		
+	}
+}
