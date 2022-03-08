69866750281432ef0a5a97e143f280372c190ac3
S-Kurisu <62637568+S-Kurisu@users.noreply.github.com>
2022-03-07 17:46:00 -0800

    Moving files to main
    moving files

---
+++ java/src/Messages.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Messages extends SubScreen
+{
+	/**
+	 * Default Constructor MathDepartment subclass
+	 */
+	Messages()
+	{
+		
+	}
+	/**
+	 * A method that reads in the info from the Math text file
+	 * to display in the main text area as information for the 
+	 * Math department
+	 */
+	public void display()
+	{
+
+	}
+}
