ed7d283ffda7725a570119567195dd70873c8adc
Shawn Long <slong024@ucr.edu>
2022-03-13 20:04:53 -0700

    removed unnecessary files and modified create_db and load_data

---
+++ sql/src/load_data.sql
@@ -0,0 +1,19 @@
+COPY USR
+FROM 'message.csv'
+WITH DELIMITER ',';
+
+COPY WORK_EXPR
+FROM 'work_expr.csv'
+WITH DELIMITER ',';
+
+COPY EDUCATIONAL_DETAILS
+FROM 'educational_details.csv'
+WITH DELIMITER ',';
+
+COPY MESSAGE
+FROM 'message.csv'
+WITH DELIMITER ',';
+
+COPY CONNECTION_USR
+FROM 'connection_usr.csv'
+WITH DELIMITER ',';
